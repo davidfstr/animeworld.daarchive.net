5f149891b79746cb6c9ebf9a9834c0ce1ab01db2
David Foster <david@dafoster.net>
2025-09-12 20:17:48 -0400

    Publish

---
+++ contact/index.php
@@ -0,0 +1,184 @@
+ <!DOCTYPE html>
+
+<html lang="en">
+<head><meta content="noindex,nofollow" name="robots"/>
+<!--[if IE]><![endif]-->
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+<meta content="The Contact page Akemi's Anime World." name="description"/>
+<meta content="" name="keywords"/>
+<title>Contact Akemi's Anime World : Site Info : AAW</title>
+<!--[if IE]><![if gte IE 8]><![endif]-->
+<link href="../stylesheets/mainstyle_v31.css" id="fullcss" media="screen, projection" rel="stylesheet" type="text/css"/>
+<link href="../stylesheets/mobile_style_v31.css" id="mobilecss" media="only handheld" rel="stylesheet" type="text/css"/>
+<!--[if IE]><![endif]><![endif]-->
+<!--[if IE 7]>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/old_ie_v30.css" media="screen" />
+	<![endif]-->
+<!--[if IE 6]>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/really_old_ie_v30.css" media="screen" />
+	<![endif]-->
+<meta content="width=520px" name="viewport"/><script src="../javascript/aawspecific-standalone-v10.yui.js" type="text/javascript"></script>
+<script type="text/javascript">
+set_css_from_cookie();
+</script>
+<!-- Project Wonderful Ad Box Loader -->
+<!-- Put this after the <body> tag at the top of your page -->
+<script type="text/javascript">
+   (function(){function pw_load(){
+      if(arguments.callee.z)return;else arguments.callee.z=true;
+      var d=document;var s=d.createElement('script');
+      var x=d.getElementsByTagName('script')[0];
+      s.type='text/javascript';s.async=true;
+      s.src='../_/http/www.projectwonderful.com/pwa.js';
+      x.parentNode.insertBefore(s,x);}
+   if (window.attachEvent){
+    window.attachEvent('DOMContentLoaded',pw_load);
+    window.attachEvent('onload',pw_load);}
+   else{
+    window.addEventListener('DOMContentLoaded',pw_load,false);
+    window.addEventListener('load',pw_load,false);}})();
+</script>
+<!-- End Project Wonderful Ad Box Loader --></head>
+<body>
+<div id="main">
+<header><div id="titlecontainer">
+<div id="titlestripe"><a href="../index.html"><img alt="Akemi's Anime World" class="sitetitle" height="80" src="../_/http/media.animeworld.org/sitegraphics/akemisanimeworld-24alpha.png" width="326"/></a></div>
+</div></header>
+<div id="contentcontainer">
+<div id="content">
+<article><div class="article">
+<h1>Contact <span class="sectionname">AAW</span></h1>
+<p class="fulltitle">Send us your questions, complaints, useful information, or anything else.</p>
+<div id="interlinks">
+<nav>
+<h2>On This Page:</h2>
+<!--[if IE]><![if gte IE 8]><![endif]-->
+<p class="styleswitcher"><a href="index.php" id="full" onclick="switch_full();return false;">Switch to Normal Layout</a> <a href="index.php" id="mobile" onclick="switch_mobile();return false;">Switch to Mobile Layout</a></p>
+<!--[if IE]><![endif]><![endif]-->
+<ul>
+<li><a href="#titlestripe">Page Top</a></li><li class="mobileonly"><a href="#menu">Site Menu</a></li>
+</ul>
+</nav>
+</div>
+<ul class="byline">
+<li>Updated: <strong>2010-03-12</strong></li>
+</ul>
+<p>Everything we get will be read, and we do our best to respond if we can, but we do recieve a lot of questions and suggestions, and there is a limit to what we can do.</p>
+<p>You can also send email to <a href="mailto:questions@animeworld.com">questions@animeworld.com</a>, but due to the preposterous amounts of spam we get there is a chance your message might get accidentally filtered, so you're usually better off using this form.  Also, if you're going to send us an attachment, send a note to warn us first so it doesn't get deleted along with the hail of viruses.</p>
+<!-- START of Dagon Design Formmailer output -->
+<div class="ddfmwrap"><form action="index.php" class="ddfm" enctype="multipart/form-data" method="post">
+<p class="fieldwrap"><label for="fm_name"><span class="required">*</span> Your Name:</label><input class="fmtext" id="fm_name" name="fm_name" type="text" value=""/></p>
+<p class="fieldwrap"><label for="fm_email">Your E-Mail Address:</label><input class="fmtext" id="fm_email" name="fm_email" type="text" value=""/></p>
+<p class="fieldwrap"><label>Why You're Writing:</label><select class="fmselect" code="" id="fm_subject" name="fm_subject">
+<option>Contest Entry</option>
+<option>Comment</option>
+<option>Suggestion</option>
+<option>Question</option>
+<option>Just Saying Hi</option>
+<option>Commercial Enquiry</option>
+<option>Other</option>
+</select></p>
+<p class="fieldwrap"><label class="fmtextlblwide" for="fm_message"><span class="required">*</span> Your Suggestion/Comment:</label>
+<textarea class="fmtextarea" cols="50" id="fm_message" name="fm_message" rows="8"></textarea></p>
+<div class="optional"><p class="fieldwrap">
+This is to make sure you're not a robot sending spam--sorry!
+</p></div>
+<script>
+				var RecaptchaOptions = {
+			    theme : 'white'
+				};
+				</script><div class="recaptcha"><div class="recaptcha-inner"><script src="../_/http/api.recaptcha.net/challenge$/k=6Ldo5gEAAAAAAEILk51qsejXcruYVRg5lX6Zo4aU/index.html" type="text/javascript"></script>
+<noscript>
+<iframe frameborder="0" height="300" src="../_/http/api.recaptcha.net/noscript$/k=6Ldo5gEAAAAAAEILk51qsejXcruYVRg5lX6Zo4aU/index.html" width="500"></iframe><br/>
+<textarea cols="40" name="recaptcha_challenge_field" rows="3"></textarea>
+<input name="recaptcha_response_field" type="hidden" value="manual_challenge"/>
+</noscript></div></div><p><input name="MAX_FILE_SIZE" type="hidden" value="1000000"/></p>
+<div class="submit"><input name="form_submitted" type="submit" value="Send Email"/></div>
+<div class="credits">Script by <a href="../_/http/www.dagondesign.com/index.html" title="Dagon Design">Dagon Design</a></div>
+</form></div>
+<!-- END of Dagon Design Formmailer output -->
+</div></article>
+</div>
+</div>
+<div id="righthandstuff">
+<div id="menu">
+<nav>
+<a class="logo" href="../index.html"><img alt="AAW" class="logo" height="100" src="../_/http/media.animeworld.org/sitegraphics/aaw-logo-alpha.png" width="172"/></a>
+<div id="searchbox">
+<form action="../search/reviewsearch.cgi" id="sitesearch" method="post">
+<p>
+<input name="dbname" type="hidden" value="reviewtitles.data"/>
+<input name="action" type="hidden" value="searchdbdisplay"/>
+<input name="template" type="hidden" value="template.html"/>
+<input name="keywords_separator_is_comma" type="hidden" value="yes"/>
+<input autofocus="" id="sitesearchbox" maxlength="100" name="keywords" size="35" type="search"/><br/>
+<input id="sitesearchbutton" type="submit" value="Review Title Search"/>
+</p>
+</form>
+</div>
+<h2 class="hidden">Site Navigation</h2>
+<ul class="menulevel1"><li><h3>Reviews</h3>
+<ul class="menulevel2">
+<li><a href="../reviews/index.html">anime by title</a></li>
+<li><a href="../starindex/index.html">anime by star rating</a></li>
+<li><a href="../related/index.html">anime by content</a></li>
+<li><a href="../morereviews/index.html">more</a></li>
+</ul>
+</li><li><h3>Reference</h3>
+<ul class="menulevel2">
+<li><a href="../reference/animefaq.html">anime FAQ</a></li>
+<li><a href="../reference/glossary.html">anime glossary</a></li>
+<li><a href="../_/http/reference.animeworld.com/index.html">Japanese dictionary</a></li>
+<li><a href="../companies/index.html">US anime companies</a></li>
+<li><a href="../reference/links.html">favorite links</a></li>
+</ul>
+</li><li><h3>Lessons</h3>
+<ul class="menulevel2">
+<li><a href="../japanese/index.html">Japanese</a></li>
+<li><a href="../howtodraw/index.html">how to draw</a></li>
+<li><a href="../editorials/howtomakeafansite.html">fansite how-to</a></li>
+</ul>
+</li><li><h3>Writing</h3>
+<ul class="menulevel2">
+<li><a href="../editorials/index.html">editorials</a></li>
+<li><a href="../humor/index.html">humor</a></li>
+<li><a href="../indie/index.html">indie anime</a></li>
+</ul>
+</li><li><h3>Fun</h3>
+<ul class="menulevel2">
+<li><a href="../_/http/forum.animeworld.com/index.html">forums</a></li>
+<li><a href="../_/http/blog.animeworld.com/index.html">blog</a></li>
+<li><a href="../contest/index.html">contests</a></li>
+<li><a href="../photos/index.html">photos</a></li>
+<li><a href="../lyrics/index.html">song lyrics</a></li>
+<li><a href="../topten.html">top 10s</a></li>
+<li><a href="../mac/index.html">MacFace</a></li>
+</ul>
+</li><li><h3>Site</h3>
+<ul class="menulevel2">
+<li><a href="../aboutaaw.html">about</a></li>
+<li><a href="../sitehistory.html">history</a></li>
+<li><a href="../siteconventions.html">review conventions</a></li>
+<li><a href="../sitefaq.html">faq</a></li>
+<li><a href="../store/index.html">store</a></li>
+<li><a class="menuselection solomenuselect" href="index.html">contact</a></li>
+</ul>
+</li></ul>
+</nav>
+<br style="clear:both;"/>
+</div>
+<aside>
+<div class="advertising"></div>
+</aside>
+</div>
+</div>
+<footer>
+<div id="footer">
+<p>Questions or comments? <a href="index.html">Contact us</a>.<br>
+©1997-2011 Marc &amp; Akemi Marshall.  Page generated <!--WMDepIgnore-->Sat Nov 22 10:22:00 2014<!--/WMDepIgnore--> <img alt="counter" height="5" src="../_/http/counter.dreamhost.com/cgi-bin/Count.cgi$/df=nnsite.dat&amp;dd=E&amp;istrip=T&amp;pad=F&amp;ft=0.cgi" width="10"/><br/>
+	Any images are the property of their respective rightsholders, and included for review purposes only under the fair-use clause of US copyright law.</br></p>
+</div>
+</footer>
+<a href="https://dafoster.net/projects/crystal-web-archiver/" id="cr-footer-banner" style="border-top: 2px #B40010 solid;background: #FFFAE1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;font-variant: initial;font-weight: initial;text-transform: none;font-size: 14px;color: #6c757d;line-height: 2.0;cursor: pointer;display: flex;align-items: center;justify-content: center;gap: 4px;clear: both;" target="_blank"><img height="24" onerror="this.style['display'] = 'none';" src="../_/crystal/resources/appicon.png" width="24"/><span>This page was archived with Crystal</span><script>window.addEventListener('load', function() { const $1 = document.querySelector('#cr-footer-banner'); if (!$1) { return; } if (window !== window.top) { let $2 = false; if (window.name) { const $3 = window.parent.document.getElementsByName(window.name); if ($3.length === 1) { const $4 = $3[0]; if ($4.tagName === 'FRAME' && $4.parentElement.tagName === 'FRAMESET') { let $5 = $4; while (true) { if ($5.parentElement.tagName !== 'FRAMESET') { $2 = true; break; } if ($5.parentElement.attributes['rows'] !== undefined) { const $6 = $5.parentElement.children; if ($5 === $6[$6.length - 1]) { $5 = $5.parentElement; continue; } else { break; } } else if ($5.parentElement.attributes['cols'] !== undefined) { const $7 = Array.from($5.parentElement.children); const $8 = $7.indexOf($5); if ($8 === -1) { break; } const $9 = $5.parentElement.attributes['cols'].value.split(','); const $10 = $9.map((s) => parseInt(s.trim())); if ($9[$8].trim() === '*' || $10[$8] === Math.max.apply(null, $10)) { $5 = $5.parentElement; continue; } else { break; } } else { break; } } } } } if (!$2) { $1.style['display'] = 'none'; } } const $11 = $1.getBoundingClientRect(); const $12 = ($11.y < window.innerHeight - $11.height); if ($12) { $1.style['position'] = 'fixed'; $1.style['bottom'] = '0'; $1.style['left'] = '0'; $1.style['right'] = '0'; $1.style['z-index'] = '9999'; } const $13 = ( document.body.getBoundingClientRect().height < $11.height * 2 ); if ($13) { $1.style['display'] = 'none'; }
+});</script></a></body>
+</html>
---
+++ sitesearch.php
@@ -0,0 +1,159 @@
+ <!DOCTYPE html>
+
+<html lang="en">
+<head><meta content="noindex,nofollow" name="robots"/>
+<!--[if IE]><![endif]-->
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+<meta content="Search for stuff at Akemi's Anime World." name="description"/>
+<meta content="search,find" name="keywords"/>
+<title>Search Results : Site Search : AAW</title>
+<!--[if IE]><![if gte IE 8]><![endif]-->
+<link href="stylesheets/mainstyle_v31.css" id="fullcss" media="screen, projection" rel="stylesheet" type="text/css"/>
+<link href="stylesheets/mobile_style_v31.css" id="mobilecss" media="only handheld" rel="stylesheet" type="text/css"/>
+<!--[if IE]><![endif]><![endif]-->
+<!--[if IE 7]>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/old_ie_v30.css" media="screen" />
+	<![endif]-->
+<!--[if IE 6]>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/really_old_ie_v30.css" media="screen" />
+	<![endif]-->
+<meta content="width=520px" name="viewport"/>
+<link href="_/http/www.google.com/cse/style/look/minimalist.css" rel="stylesheet" type="text/css"/>
+<script src="javascript/aawspecific-standalone-v10.yui.js" type="text/javascript"></script>
+<script type="text/javascript">
+set_css_from_cookie();
+</script>
+<!-- Project Wonderful Ad Box Loader -->
+<!-- Put this after the <body> tag at the top of your page -->
+<script type="text/javascript">
+   (function(){function pw_load(){
+      if(arguments.callee.z)return;else arguments.callee.z=true;
+      var d=document;var s=d.createElement('script');
+      var x=d.getElementsByTagName('script')[0];
+      s.type='text/javascript';s.async=true;
+      s.src='_/http/www.projectwonderful.com/pwa.js';
+      x.parentNode.insertBefore(s,x);}
+   if (window.attachEvent){
+    window.attachEvent('DOMContentLoaded',pw_load);
+    window.attachEvent('onload',pw_load);}
+   else{
+    window.addEventListener('DOMContentLoaded',pw_load,false);
+    window.addEventListener('load',pw_load,false);}})();
+</script>
+<!-- End Project Wonderful Ad Box Loader --></head>
+<body>
+<div id="main">
+<header><div id="titlecontainer">
+<div id="titlestripe"><a href="index.html"><img alt="Akemi's Anime World" class="sitetitle" height="80" src="_/http/media.animeworld.org/sitegraphics/akemisanimeworld-24alpha.png" width="326"/></a></div>
+</div></header>
+<div id="contentcontainer">
+<div id="content">
+<article><div class="article">
+<h1>AAW Site Search <span class="sectionname">Search Results</span></h1>
+<p class="fulltitle">Find stuff at AAW.</p>
+<div id="interlinks">
+<nav>
+<h2>On This Page:</h2>
+<!--[if IE]><![if gte IE 8]><![endif]-->
+<p class="styleswitcher"><a href="sitesearch.php" id="full" onclick="switch_full();return false;">Switch to Normal Layout</a> <a href="sitesearch.php" id="mobile" onclick="switch_mobile();return false;">Switch to Mobile Layout</a></p>
+<!--[if IE]><![endif]><![endif]-->
+<ul>
+<li><a href="#titlestripe">Page Top</a></li><li class="mobileonly"><a href="#menu">Site Menu</a></li>
+</ul>
+</nav>
+</div>
+<form action="sitesearch.php" id="searchform">
+<div>
+<input name="search" size="40" type="text" value=""/> <input type="submit" value="Search"/>
+</div>
+</form>
+</div></article>
+</div>
+</div>
+<div id="righthandstuff">
+<div id="menu">
+<nav>
+<a class="logo" href="index.html"><img alt="AAW" class="logo" height="100" src="_/http/media.animeworld.org/sitegraphics/aaw-logo-alpha.png" width="172"/></a>
+<div id="searchbox">
+<form action="search/reviewsearch.cgi" id="sitesearch" method="post">
+<p>
+<input name="dbname" type="hidden" value="reviewtitles.data"/>
+<input name="action" type="hidden" value="searchdbdisplay"/>
+<input name="template" type="hidden" value="template.html"/>
+<input name="keywords_separator_is_comma" type="hidden" value="yes"/>
+<input autofocus="" id="sitesearchbox" maxlength="100" name="keywords" size="35" type="search"/><br/>
+<input id="sitesearchbutton" type="submit" value="Review Title Search"/>
+</p>
+</form>
+</div>
+<h2 class="hidden">Site Navigation</h2>
+<ul class="menulevel1"><li><h3>Reviews</h3>
+<ul class="menulevel2">
+<li><a href="reviews/index.html">anime by title</a></li>
+<li><a href="starindex/index.html">anime by star rating</a></li>
+<li><a href="related/index.html">anime by content</a></li>
+<li><a href="morereviews/index.html">more</a></li>
+</ul>
+</li><li><h3>Reference</h3>
+<ul class="menulevel2">
+<li><a href="reference/animefaq.html">anime FAQ</a></li>
+<li><a href="reference/glossary.html">anime glossary</a></li>
+<li><a href="_/http/reference.animeworld.com/index.html">Japanese dictionary</a></li>
+<li><a href="companies/index.html">US anime companies</a></li>
+<li><a href="reference/links.html">favorite links</a></li>
+</ul>
+</li><li><h3>Lessons</h3>
+<ul class="menulevel2">
+<li><a href="japanese/index.html">Japanese</a></li>
+<li><a href="howtodraw/index.html">how to draw</a></li>
+<li><a href="editorials/howtomakeafansite.html">fansite how-to</a></li>
+</ul>
+</li><li><h3>Writing</h3>
+<ul class="menulevel2">
+<li><a href="editorials/index.html">editorials</a></li>
+<li><a href="humor/index.html">humor</a></li>
+<li><a href="indie/index.html">indie anime</a></li>
+</ul>
+</li><li><h3>Fun</h3>
+<ul class="menulevel2">
+<li><a href="_/http/forum.animeworld.com/index.html">forums</a></li>
+<li><a href="_/http/blog.animeworld.com/index.html">blog</a></li>
+<li><a href="contest/index.html">contests</a></li>
+<li><a href="photos/index.html">photos</a></li>
+<li><a href="lyrics/index.html">song lyrics</a></li>
+<li><a href="topten.html">top 10s</a></li>
+<li><a href="mac/index.html">MacFace</a></li>
+</ul>
+</li><li><h3 class="menuselection">Site</h3>
+<ul class="menulevel2">
+<li><a href="aboutaaw.html">about</a></li>
+<li><a href="sitehistory.html">history</a></li>
+<li><a href="siteconventions.html">review conventions</a></li>
+<li><a href="sitefaq.html">faq</a></li>
+<li><a href="store/index.html">store</a></li>
+<li><a href="contact/index.html">contact</a></li>
+</ul>
+</li></ul>
+</nav>
+<br style="clear:both;"/>
+</div>
+<aside>
+<div class="advertising"><div class="sidebox ads">
+<h2>Ads</h2>
+<p>
+<img alt="Fake advertising" height="280" src="_/http/media.animeworld.org/sitegraphics/fakeads/ad_bakahammer2.png" width="340"/>
+</p>
+</div></div>
+</aside>
+</div>
+</div>
+<footer>
+<div id="footer">
+<p>Questions or comments? <a href="contact/index.html">Contact us</a>.<br>
+©1997-2011 Marc &amp; Akemi Marshall.  Page generated <!--WMDepIgnore-->Sat Nov 22 10:21:52 2014<!--/WMDepIgnore--> <img alt="counter" height="5" src="_/http/counter.dreamhost.com/cgi-bin/Count.cgi$/df=nnsite.dat&amp;dd=E&amp;istrip=T&amp;pad=F&amp;ft=0.cgi" width="10"/><br/>
+	Any images are the property of their respective rightsholders, and included for review purposes only under the fair-use clause of US copyright law.</br></p>
+</div>
+</footer>
+<a href="https://dafoster.net/projects/crystal-web-archiver/" id="cr-footer-banner" style="border-top: 2px #B40010 solid;background: #FFFAE1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;font-variant: initial;font-weight: initial;text-transform: none;font-size: 14px;color: #6c757d;line-height: 2.0;cursor: pointer;display: flex;align-items: center;justify-content: center;gap: 4px;clear: both;" target="_blank"><img height="24" onerror="this.style['display'] = 'none';" src="_/crystal/resources/appicon.png" width="24"/><span>This page was archived with Crystal</span><script>window.addEventListener('load', function() { const $1 = document.querySelector('#cr-footer-banner'); if (!$1) { return; } if (window !== window.top) { let $2 = false; if (window.name) { const $3 = window.parent.document.getElementsByName(window.name); if ($3.length === 1) { const $4 = $3[0]; if ($4.tagName === 'FRAME' && $4.parentElement.tagName === 'FRAMESET') { let $5 = $4; while (true) { if ($5.parentElement.tagName !== 'FRAMESET') { $2 = true; break; } if ($5.parentElement.attributes['rows'] !== undefined) { const $6 = $5.parentElement.children; if ($5 === $6[$6.length - 1]) { $5 = $5.parentElement; continue; } else { break; } } else if ($5.parentElement.attributes['cols'] !== undefined) { const $7 = Array.from($5.parentElement.children); const $8 = $7.indexOf($5); if ($8 === -1) { break; } const $9 = $5.parentElement.attributes['cols'].value.split(','); const $10 = $9.map((s) => parseInt(s.trim())); if ($9[$8].trim() === '*' || $10[$8] === Math.max.apply(null, $10)) { $5 = $5.parentElement; continue; } else { break; } } else { break; } } } } } if (!$2) { $1.style['display'] = 'none'; } } const $11 = $1.getBoundingClientRect(); const $12 = ($11.y < window.innerHeight - $11.height); if ($12) { $1.style['position'] = 'fixed'; $1.style['bottom'] = '0'; $1.style['left'] = '0'; $1.style['right'] = '0'; $1.style['z-index'] = '9999'; } const $13 = ( document.body.getBoundingClientRect().height < $11.height * 2 ); if ($13) { $1.style['display'] = 'none'; }
+});</script></a></body>
+</html>
